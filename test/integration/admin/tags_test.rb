02eff0c95cf4c572c9b8ccd7d534cf58ae4f14a4
Federico Ramirez <fedekiller@gmail.com>
2014-12-12 22:20:49 -0300

    Fix test

--- test/integration/admin/tags_test.rb
+++ test/integration/admin/tags_test.rb
@@ -1,16 +1,34 @@
 require 'test_helper'
 
 class AdminPendingTagsWorkflowTest < ActionDispatch::IntegrationTest
-  test 'can approve a tag' do
+  test 'can approve a tag as fandom' do
     user = FactoryGirl.create(:user_admin)
     login_as user
 
     tag = FactoryGirl.create(:tag)
+    tag.context = 'fandoms'
+
+    assert 'pending', tag.status
 
     visit admin_tags_url
     has_content? tag.name
 
-    find("##{tag.id}-actions .approve").click
+    find("##{tag.id}-actions .approve-fandom").click
+    assert 'active', tag.reload.status
+  end
+
+  test 'can approve a tag as character' do
+    user = FactoryGirl.create(:user_admin)
+    login_as user
+
+    tag = FactoryGirl.create(:tag) # is context characters by default
+
+    assert 'pending', tag.status
+
+    visit admin_tags_url
+    has_content? tag.name
+
+    find("##{tag.id}-actions .approve-character").click
     assert 'active', tag.reload.status
   end
 
