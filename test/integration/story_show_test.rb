c6227aa1a6d8b467d65311c53844365bc93d4e34
Federico Ramirez <fedekiller@gmail.com>
2014-12-12 22:22:49 -0300

    Fix story test

--- test/integration/story_show_test.rb
+++ test/integration/story_show_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class StoryShowTest < ActionDispatch::IntegrationTest
   test 'stories should increase view count' do
-    story = FactoryGirl.create(:story)
+    story = FactoryGirl.create(:story_published)
     assert_equal 0, story.views
     
     visit story_url(story)
-    assert_equal story_path(story), current_path
 
+    assert_equal story_path(story), current_path
     assert_equal 1, story.reload.views
   end
 end
