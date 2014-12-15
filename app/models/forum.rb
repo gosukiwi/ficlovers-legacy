de367721d318da5f803c1ec6dd7d8d695d34fef5
Federico Ramirez <fedekiller@gmail.com>
2014-12-15 02:07:42 -0300

    Prettier forum urls

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -2,4 +2,8 @@ class Forum < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
 
   has_many :posts
+
+  def to_param
+    "#{id}-#{name.parameterize}"
+  end
 end
