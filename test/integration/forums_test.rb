a9ad482db64732ce6d8930a7b2df4b8aaac73321
Federico Ramirez <fedekiller@gmail.com>
2014-12-17 02:15:18 -0300

    Oops forgot new test file

---
+++ test/integration/forums_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class ForumsTest < ActionDispatch::IntegrationTest
+  test 'user can see new-post button' do
+    forum = FactoryGirl.create(:forum)
+    user = FactoryGirl.create(:user)
+
+    login_as user
+    visit forum_url(forum)
+
+    find('.new-post').click
+
+    assert_equal new_forum_post_path(forum), current_path
+  end
+
+  test 'anon cannot see new-post button' do
+    forum = FactoryGirl.create(:forum)
+    user = FactoryGirl.create(:user)
+
+    visit forum_url(forum)
+
+    assert has_no_selector?('.new-post')
+  end
+
+  test 'forums posts are beeing displayed' do
+    post = FactoryGirl.create(:post)
+
+    visit forum_url(post.forum)
+
+    assert_equal post.title, find('.posts-table a').text
+  end
+end
