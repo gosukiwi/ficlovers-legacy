5beba0c38568707c920a62306610c2bfd54005ad
Federico Ramirez <fedekiller@gmail.com>
2014-12-24 16:09:05 -0300

    Fix show page

--- app/helpers/has_thumbnail_helper.rb
+++ app/helpers/has_thumbnail_helper.rb
@@ -1,11 +1,10 @@
 module HasThumbnailHelper
   def thumb_url_for(story)
-    StoryThumb.new(story).thumb_url
+    story.thumb_url
   end
 
   def thumb_background_for(story)
-    url = StoryThumb.new(story).thumb_url
-    return '' unless url
-    "background: url(#{url}) 0 50%; background-size: cover;"
+    return '' unless story.thumb_url
+    "background: url(#{story.thumb_url}) 0 50%; background-size: cover;"
   end
 end
