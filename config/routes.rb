0481ad268fb204ee3cf27a0177df9e5c6bf70327
Federico Ramirez <fedekiller@gmail.com>
2014-12-24 09:13:10 -0300

    Work on settings frontend

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     get 'search', on: :collection
     put 'update_tags', to: 'stories#update_tags', on: :member
     get 'settings', to: 'stories#settings', on: :member
+    post 'upload', to: 'stories#upload', on: :member
   end
   get 'fresh', to: 'stories#fresh', as: :fresh_stories
   get 'hot', to: 'stories#hot', as: :hot_stories
