5956d0e003871390e62f01b23e7f1b38a8e2c350
Federico Ramirez <fedekiller@gmail.com>
2015-01-01 01:59:26 -0300

    Use attr_reader instead of direct access

--- app/actions/notify_user.rb
+++ app/actions/notify_user.rb
@@ -1,10 +1,11 @@
 class NotifyUser
+  attr_reader :user, :message
   def initialize(user, message)
     @user = user
     @message = message
   end
 
   def notify
-    Notification.create(user: @user, message: @message)
+    Notification.create(user: user, message: message)
   end
 end
