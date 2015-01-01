9b13e297aa68fd37d60523338e297589b17edfa7
Federico Ramirez <fedekiller@gmail.com>
2015-01-01 00:00:34 -0300

    Make S3 more loosely dependant

--- app/services/s3_service.rb
+++ app/services/s3_service.rb
@@ -1,14 +1,28 @@
 gem 'aws-sdk'
 
 class S3Service
-  def initialize
-    AWS.config access_key_id: ENV['FANFIC_S3_KEY'], secret_access_key: ENV['FANFIC_S3_SECRET']
-    @bucket = AWS::S3.new.buckets[ENV['FANFIC_S3_BUCKET']]
+  def bucket
+    @bucket ||= default_bucket
+  end
+
+  def get(name)
+    bucket.objects["thumbs/#{name}"]
+  end
+
+  def url(obj)
+    "https://#{obj.bucket.name}.s3.amazonaws.com/#{obj.key}"
   end
 
   def put(file, name)
-    obj = @bucket.objects["thumbs/#{name}"]
-    result = obj.write file, acl: :public_read
-    "https://#{result.bucket.name}.s3.amazonaws.com/#{result.key}"
+    obj = get name
+    obj.write file, acl: :public_read
+    url obj
   end
+
+  protected
+
+    def default_bucket
+      AWS.config access_key_id: ENV['FANFIC_S3_KEY'], secret_access_key: ENV['FANFIC_S3_SECRET']
+      AWS::S3.new.buckets[ENV['FANFIC_S3_BUCKET']]
+    end
 end
