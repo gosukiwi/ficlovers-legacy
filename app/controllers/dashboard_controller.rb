5f23f33caad40cd2e8b134230fafa4fbe0530088
Federico Ramirez <fedekiller@gmail.com>
2015-01-22 04:00:10 -0300

    Show 2 stories in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   def index
     @user = current_user
     @notifications = policy_scope(Notification).sorted.limit(5)
-    @feed = @user.feed.limit(5)
+    @feed = @user.feed.limit(2)
   end
 end
