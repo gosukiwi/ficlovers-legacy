66fcfc9fc8bfe509d784ddb3fc38395c5301e25c
Federico Ramirez <federico_r@beezwax.net>
2015-10-30 00:31:39 -0300

    Remove old code

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -2,14 +2,10 @@ class ForumsController < ApplicationController
   before_action :set_forum, only: [:show]
   def index
     @forum = ForumFacade.new page: params[:page]
-    #@forums = Forum.all
-    #@posts  = Post.latest.paginate(page: params[:page], per_page: 25)
   end
 
   def show
     @forum = ForumFacade.new forum: @forum, page: params[:page]
-    #@forums = Forum.all
-    #@posts  = @forum.posts.sorted.paginate(page: params[:page], per_page: 25)
   end
 
   private
