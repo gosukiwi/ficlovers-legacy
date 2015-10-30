6ca0040dd6d2e0173ef485394cd1f5a85bf218ea
Federico Ramirez <federico_r@beezwax.net>
2015-10-30 01:11:32 -0300

    DRY up code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,52 +1,50 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :pin, :watch]
   before_action :set_forum
+  before_action :set_post, only: [:show, :edit, :update, :pin, :watch]
+  before_action :check_authorization
   before_action :increment_views, only: [:show]
 
   def watch
-    authorize @post
     @result = current_user.watch_toggle @post
   end
 
   def show
-    authorize @post
     @post = PostFacade.new post: @post, page: params[:page]
   end
 
   def new
     @post = Post.new forum: @forum
-    authorize @post
   end
 
   def create
     @post = Post.new post_params
-    authorize @post
     if @post.save
-      redirect_to [@forum, @post], notice: 'Post was successfully created.'
+      redirect_to [@forum, @post]
     else
       render :new
     end
   end
 
   def edit
-    authorize @post
   end
 
   def update
-    authorize @post
     if @post.update(post_params)
-      redirect_to [@forum, @post], notice: 'Post was successfully created.'
+      redirect_to [@forum, @post]
     else
       render :edit
     end
   end
 
   def pin
-    authorize @post
     @success = @post.toggle! :pinned
   end
 
   private
+  
+  def check_authorization
+    authorize @post || Post.new(forum: @forum)
+  end
 
   def set_post
     @post = Post.find(params[:id])
